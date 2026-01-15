8a7980f6276fe1cb2e2d95e482b333a6a9adf779
wanxiaokang <251722976@qq.com>
2026-01-15 17:26:11 +0800

    更新返回类

--- server/src/main/java/com/dingdong/common/Result.java
+++ server/src/main/java/com/dingdong/common/Result.java
@@ -8,18 +8,21 @@ public class Result<T> {
     private String msg;
     private T data;
 
+    public Result(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
     public static <T> Result<T> success(T data) {
-        Result<T> r = new Result<>();
-        r.code = 200;
-        r.msg = "success";
-        r.data = data;
-        return r;
+        return new Result<>(200, "success", data);
     }
 
     public static <T> Result<T> error(String msg) {
-        Result<T> r = new Result<>();
-        r.code = 500;
-        r.msg = msg;
-        return r;
+        return new Result<>(500, msg, null);
+    }
+
+    public static <T> Result<T> error(Integer code, String msg) {
+        return new Result<>(code, msg, null);
     }
 }
